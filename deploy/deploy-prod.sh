55e41c00a60cfbb2e7c0209dbf5d3f75d6db4de6
Conor Mc Govern <conor@mcgov.ie>
2022-10-23 19:50:57 +0100

    workflow fix

--- deploy/deploy-prod.sh
+++ deploy/deploy-prod.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Change version of API
-if [ ! -z "$1" ]
-then
-    exit 1
-fi
-export API_VERSION=$1
-
 # Stop the docker container currently running the API
 echo -e "\n > Shutting down CompSoc API"
 docker-compose -f docker-compose.prod.yml down
