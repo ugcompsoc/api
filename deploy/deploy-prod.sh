2ce47cf69aca3cf2446ac81630f131b1b6853c71
Conor Mc Govern <conor@mcgov.ie>
2022-10-23 19:54:58 +0100

    workflow fix

--- deploy/deploy-prod.sh
+++ deploy/deploy-prod.sh
@@ -4,6 +4,11 @@
 echo -e "\n > Shutting down CompSoc API"
 docker-compose -f docker-compose.prod.yml down
 
+# Pull down any new workflow related stuff
+echo -e "\n > Pulling down new updates from main"
+git checkout main
+git pull 
+
 # Start the service again
 echo -e "\n > Starting CompSoc API"
 docker-compose -f docker-compose.prod.yml up -d
