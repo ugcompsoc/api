db9398b457e28a0602522bf6936872d86dede065
Conor Mc Govern <conor@mcgov.ie>
2022-10-24 21:07:33 +0100

    add missing compose file

--- deploy/deploy-prod.sh
+++ deploy/deploy-prod.sh
@@ -5,7 +5,7 @@ echo -e "\n > Shutting down CompSoc API"
 docker-compose -f docker-compose.prod.yml down
 
 # Update image
-docker-compose pull
+docker-compose -f docker-compose.prod.yml pull
 
 # Pull down any new workflow related stuff
 echo -e "\n > Pulling down new updates from main"
